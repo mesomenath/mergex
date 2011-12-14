8c6c60f2a2f31d94341d24770feca1894684f4eb
bbeirnaert@gmail.com <bbeirnaert@gmail.com@d34a23e2-8add-c0f4-879b-833d45954800>
2011-12-14 09:31:15 +0000

    Implement possibility to add attribute to the parent root if -p is used.

--- trunk/mergex.cpp
+++ trunk/mergex.cpp
@@ -12,27 +12,32 @@ bool fexists(string filename) {
 
 int main(int argc, char* argv[]) {
 	if (argc < 2) {
-		cout << argv[0] << " v2.0:" << endl << "\tMerge XML files to std output or an output file."<<endl<<"\tALL XML should have the same xml prolog and root node." << endl;
+		cout << argv[0] << " v2.1:" << endl << "\tMerge XML files to std output or an output file."<<endl<<"\tALL XML should have the same xml prolog and root node." << endl;
 		cout << endl << "Syntax:" <<endl << "\t" << argv[0] << " PARAMS <XML_FILE> <XML_FILE> ..." << endl;
 		cout << endl << "Parameters:"<<endl;
 		cout << endl << "\t-o<OUTPUT_XML_FILE>\tPath of the XML output file." << endl;
 		cout << endl << "\t-p[ParentRootName]\tAdd a parent root. If ParentRootName is not provided,\n\t\t\t\tthe name of a root postfixed with an 's' is used." << endl;
+		cout << endl << "\t-a[ParentRootAttribute]\tAdd an attribute to the parent root.\n\t\t\t\tParentRootAttribute should be a valid xml attribute\n\t\t\t\tdefined with single quote.\n\t\t\t\tIt will only work with the -p parameter." << endl;
 		cout << endl << "Samples:"<<endl;
 		cout << endl << "\tmergex *.xml"<<endl;
 		cout << "\tmergex a.xml b.xml c.xml -ooutput.xml"<<endl;
 		cout << "\tmergex *.xml -p -ooutput.xml"<<endl;
 		cout << "\tmergex *.xml -pROOT"<<endl;
 		cout << "\tmergex -ooutput.xml -pROOT a.xml b.xml c.xml"<<endl;
+		cout << "\tmergex -ooutput.xml -pROOT -avalue='test' *.xml"<<endl;
+		cout << "\tmergex -ooutput.xml -p -avalue='test' -aAnotherAttribute=\'anothertest\' *.xml"<<endl;
 		return 0;
 	}
 
 	bool createParent(false);
-	string outputFilePath,firstXMLFilePath,parentRootName;
+	string outputFilePath,firstXMLFilePath,parentRootName,parentRootAttribute;
 	for (int i=1; i<argc; i++) {
 		string param(argv[i]);
 		createParent = createParent || param.compare(0, 2, "-p") == 0;
 		if (param.compare(0, 2, "-p") == 0) {
 			parentRootName = param.substr(2);
+		} else if (param.compare(0, 2, "-a") == 0) {
+			parentRootAttribute += " " + param.substr(2);
 		} else if (param.compare(0, 2, "-o") == 0) {
 			outputFilePath = param.substr(2);
 		} else if (firstXMLFilePath.empty() && fexists(param)) {
@@ -87,7 +92,7 @@ int main(int argc, char* argv[]) {
 			if (createParent) {
 				string::size_type pos;
 				if ((pos = header.find(startRootNodeKey)) != string::npos) {
-					header = header.substr(0,pos)+"<"+parentRootName+">";
+					header = header.substr(0,pos)+"<"+parentRootName+parentRootAttribute+">";
 				}
 			}
 
